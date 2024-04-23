285afc30d11ff8136bbb11c2e536da22db9092c5
JoFrhwld <JoFrhwld@gmail.com>
2024-04-23 11:07:28 -0400

    project structure

--- _usethis_hist.R
+++ _usethis_hist.R
@@ -1,3 +1,5 @@
+# Setup ----
+
 usethis::git_vaccinate()
 
 desc::desc_coerce_authors_at_r()
@@ -10,4 +12,31 @@ usethis::use_author(
 )
 
 usethis::use_roxygen_md()
-devtools::document()
+
+desc::desc_set(
+  "Title",
+  "Curve Means"
+)
+
+
+desc::desc_set(
+  "Description",
+  "These are a collection of helper functions to calculate
+  averages curve functions given a collection of basis weights.",
+  normalize = T
+)
+
+usethis::use_gpl3_license()
+
+usethis::use_readme_md()
+
+fs::file_delete(
+  here::here("NAMESPACE")
+)
+
+roxygen2::roxygenize()
+
+# imports ---
+
+usethis::use_package("R", type = "Depends", min_version = "4.1")
+usethis::use_package("gsignal")
