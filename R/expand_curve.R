6cd74eb2a661f44be653e0571a94cd1b8dbaf137
JoFrhwld <JoFrhwld@gmail.com>
2024-04-24 11:29:58 -0400

    expand_dct should only do 1d

--- R/expand_curve.R
+++ R/expand_curve.R
@@ -13,18 +13,9 @@
 #' @export
 
 expand_dct <- function(coefs, size = length(coefs)){
-  if(is.null(dim(coefs))){
-    n_basis = length(coefs)
-  }else{
-    n_basis = dim(coefs)[2]
-  }
+  n_basis = length(coefs)
   basis <- dct_basis(n_basis = n_basis, size = size)
-  fit <- coefs %*% basis
-
-  # flatten if 1d
-  if(is.null(dim(coefs))){
-    fit <- c(fit)
-  }
+  fit <- c(coefs %*% basis)
   return(fit)
 }
 
@@ -38,4 +29,24 @@ expand_dct_df <- function(coefs, size = length(coefs)){
   basis <- dct_basis(n_basis = n_basis, size = size)
   fit <- coefs %*% basis
   as.data.frame(t(fit))
+}
+
+
+#' Get the rate of change (first derivative) of DCT
+#'
+#' This will return the rate of change (first derivative)
+#' of a Discrete Cosine Transform. For large \code{size}s,
+#' the first-forward-difference of the inverse DCT will
+#' approximate the results of this function.
+#'
+#' @export
+expand_dct_rate <- function(coefs, size = length(coefs)){
+  hatu = coefs * pi
+  idx = (1:length(coefs))-1
+  hatu = -idx * hatu
+  hatu = hatu[-1]
+  hatu = c(hatu, 0)
+  dotu = gsignal::idst(hatu, n = size)
+  return(dotu)
+
 }
